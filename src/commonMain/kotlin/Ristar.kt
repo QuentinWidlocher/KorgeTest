58467624fe5223e38329ef45e03e6559a8c81bac
Quentin Widlocher <quentin.widlocher@erudo.fr>
2020-08-11 18:08:31 +0200

    WIP

--- src/commonMain/kotlin/Ristar.kt
+++ src/commonMain/kotlin/Ristar.kt
@@ -3,39 +3,34 @@ import com.soywiz.klock.seconds
 import com.soywiz.korge.view.Sprite
 import com.soywiz.korge.view.SpriteAnimation
 import com.soywiz.korge.view.Stage
-import com.soywiz.korim.bitmap.Bitmap
 import com.soywiz.korim.format.readBitmap
+import com.soywiz.korio.async.async
 import com.soywiz.korio.async.launch
 import com.soywiz.korio.file.std.resourcesVfs
 import kotlinx.coroutines.GlobalScope
 
 class Ristar(parent: Stage): MovableObject<Sprite>(parent) {
 
-    private val ANIMATION_FILES: HashMap<RistarAnimation, String> = HashMap()
-
-    private val animations: HashMap<RistarAnimation, SpriteAnimation> = HashMap()
+    private var animations: HashMap<RistarAnimation, SpriteAnimation> = HashMap()
 
     override fun init() {
-        sprite = Sprite();
-        ANIMATION_FILES.apply {
-            this[Walking] = "RistarWalking.png";
-        }
-        GlobalScope.launch(::initSprite);
+        sprite = Sprite()
+        GlobalScope.launch(::initSprite)
     }
 
     private suspend fun initSprite() {
+        animations = HashMap()
 
-        for (anim in RistarAnimation.values()) {
-            val sprite = resourcesVfs[ ANIMATION_FILES[anim]!! ].readBitmap()
-            animations[anim] = SpriteAnimation(
-                    spriteMap = sprite,
-                    spriteWidth = 44,
-                    spriteHeight = 40,
-                    rows = 1,
-                    columns = 8
-            )
+        fun loadSpriteAsync(sprite: String) = GlobalScope.async { resourcesVfs[sprite].readBitmap() }
+
+        loadSpriteAsync("RistarWalking.png").await().also {
+            animations[Walking] = SpriteAnimation(it,44, 40, 0, 0, 8, 1)
         }
 
-//        sprite.playAnimationLooped(animations[Walking], 0.15.seconds);
+        loadSpriteAsync("RistarIdle.png").await().also {
+            animations[Walking] = SpriteAnimation(it,26, 39, 0, 0, 1, 1)
+        }
+
+        sprite.playAnimationLooped(animations[Walking], 0.15.seconds)
     }
 }
--- src/commonMain/kotlin/RistarAnimation.kt
+++ src/commonMain/kotlin/RistarAnimation.kt
@@ -1,3 +1,4 @@
 enum class RistarAnimation {
+    Idle,
     Walking
 }
